b6dc7c8f88bd267af084e70b79bddba397022ad3
dayzpd <zach@dayzpd.com>
2025-07-28 11:55:52 -0500

    update bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+for arg in \"$@\"
+  do
+  case $1 in
+    --context|-c)
+      kubeconfigContext=$2
+    ;;
+    --apps|-a)
+      appsFolder=$2
+    ;;
+    --*)
+      echo "Unknown option: $1"
+      exit 1
+    ;;
+  esac
+  shift
+done
+
 set -e
 
 function installESOBase() {
 
-  argocdBase="components/external-secrets/base"
+  esoBase="components/external-secrets/base"
 
-  kubectl apply --server-side -k $argocdBase
+  kustomize build --enable-helm $esoBase | kubectl apply --server-side -f - 
 
   kubectl wait --timeout=120s -n external-secrets --for=condition=Available=True Deployment/external-secrets
   kubectl wait --timeout=120s -n external-secrets --for=condition=Available=True Deployment/external-secrets-cert-controller
@@ -19,7 +36,7 @@ function installArgoCDBase() {
 
   argocdBase="components/argocd/base"
 
-  kubectl apply --server-side -k $argocdBase
+  kubectl apply --force-conflicts --server-side -k $argocdBase
 
   kubectl wait --timeout=120s -n argocd --for=condition=Available=True Deployment/argocd-applicationset-controller
   kubectl wait --timeout=120s -n argocd --for=condition=Available=True Deployment/argocd-dex-server
@@ -34,11 +51,13 @@ function installArgoCDBase() {
 
 function installBootstrapApplication() {
 
-  kubectl apply --server-side -f apps/bootstrap.yaml
+  kubectl apply --server-side -f apps/$appsFolder/bootstrap.yaml
 
 }
 
 function main() {
+
+  kubectl config use-context $kubeconfigContext
 
   echo "Bootstrapping cluster..."
 
