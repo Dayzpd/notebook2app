5645079ece3833b0a6084918de26a3323f559351
dayzpd <zach@dayzpd.com>
2025-07-27 21:39:24 -0500

    exclude tempaltes dir in setup.py

--- pkg/setup.py
+++ pkg/setup.py
@@ -12,9 +12,10 @@ setup(
     name="notebook2app",
     version="0.1.0",
     description="Deploy Python apps to Kubernetes from within a JupyterHub notebook.",
-    author="",
-    author_email="",
-    packages=find_packages(where="src"),
+    author="Zach Day",
+    author_email="zach@dayzpd.com",
+    license="MIT",
+    packages=find_packages(where="src", exclude=["notebook2app.templates"]),
     package_dir={"": "src"},
     package_data={
         "notebook2app": ["templates/*.j2"],
@@ -23,7 +24,6 @@ setup(
     install_requires=REQUIREMENTS_TXT,
     classifiers=[
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
